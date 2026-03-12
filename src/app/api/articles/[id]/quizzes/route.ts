01d05f8e01cfd26d2aa678f16fca32e0e3269fd2
25LP7053 25LP7053 <johndoe@example.com>
2026-03-12 10:26:58 +0800

    w

--- src/app/api/articles/[id]/quizzes/route.ts
+++ src/app/api/articles/[id]/quizzes/route.ts
@@ -1,24 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-// export async function POST(request: Request) {
-
-//   const articles = await prisma.article.findMany();
-//   return NextResponse.json(articles, { status: 200 });
-//   // const { id } = params;
-//   // try {
-//   //   const article = await prisma.article.findUnique({
-//   //     where: { id },
-//   //   });
-//   //   return NextResponse.json({ article }, { status: 200 });
-//   // } catch (error) {
-//   //   return NextResponse.json(
-//   //     { error: "Failed to create quiz" },
-//   //     { status: 500 },
-//   //   );
-//   // }
-// }
-
 
 export const dynamic = "force-dynamic";
 
