2aa4913da237c948385c3ce1ddcda1ba3a1f5319
25LP7053 25LP7053 <johndoe@example.com>
2026-03-12 10:24:04 +0800

    p

--- src/app/api/articles/[id]/quizzes/route.ts
+++ src/app/api/articles/[id]/quizzes/route.ts
@@ -1,17 +1,36 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  // const { id } = params;
-  // try {
-  //   const article = await prisma.article.findUnique({
-  //     where: { id },
-  //   });
-  //   return NextResponse.json({ article }, { status: 200 });
-  // } catch (error) {
-  //   return NextResponse.json(
-  //     { error: "Failed to create quiz" },
-  //     { status: 500 },
-  //   );
-  // }
+// export async function POST(request: Request) {
+
+//   const articles = await prisma.article.findMany();
+//   return NextResponse.json(articles, { status: 200 });
+//   // const { id } = params;
+//   // try {
+//   //   const article = await prisma.article.findUnique({
+//   //     where: { id },
+//   //   });
+//   //   return NextResponse.json({ article }, { status: 200 });
+//   // } catch (error) {
+//   //   return NextResponse.json(
+//   //     { error: "Failed to create quiz" },
+//   //     { status: 500 },
+//   //   );
+//   // }
+// }
+
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  try {
+    const articles = await prisma.article.findMany();
+
+    return NextResponse.json(articles);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch articles" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -1,17 +1,32 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } },
-) {
-  // const articleId = params.id;
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { id: string } },
+// ) {
+//   // const articleId = params.id;
 
-  // const quizzes = await prisma.quiz.findMany({
-  //   where: {
-  //     articleId: articleId,
-  //   },
-  // });
+//   // const quizzes = await prisma.quiz.findMany({
+//   //   where: {
+//   //     articleId: articleId,
+//   //   },
+//   // });
 
-  // return Response.json(quizzes);
+//   // return Response.json(quizzes);
+// }
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  try {
+    const articles = await prisma.article.findMany();
+
+    return NextResponse.json(articles);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch articles" },
+      { status: 500 },
+    );
+  }
 }
